a6f1c4d98df3de3a323109d3817fb359d630806e
Muhammad Yousuf <feeds015@gmail.com>
2023-10-04 13:13:34 +0800

    persist cart state using zustand

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,18 +1,31 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
-export const useCartStore = create(set => ({
-    cartArray: [],
-    addToCart: (newItem) => set((state) => ({
-        cartArray: [...state.cartArray, {
-            id: newItem.id,
-            name: newItem.name,
-            price: newItem.price,
-            image : newItem.image
-        }]
-    })),
+export const useCartStore = create(
+  persist(
+    (set) => ({
+      cartArray: [],
+      addToCart: (newItem) =>
+        set((state) => ({
+          cartArray: [
+            ...state.cartArray,
+            {
+              id: newItem.id,
+              name: newItem.name,
+              price: newItem.price,
+              image: newItem.image
+            }
+          ]
+        })),
 
-    removeFromCart: (itemId) => set((state) => ({
-        cartArray: state.cartArray.filter((item) => itemId !== item.id)
-    }))
-
-}))
+      removeFromCart: (itemId) =>
+        set((state) => ({
+          cartArray: state.cartArray.filter((item) => itemId !== item.id)
+        }))
+    }),
+    {
+      name: "cart-storage",
+      storage: createJSONStorage(() => sessionStorage)
+    }
+  )
+);
